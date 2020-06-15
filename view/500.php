7a91ef22d0b8e9a86ff5843276ceeb290223361d
pepbustamante <49286738+pepbustamante@users.noreply.github.com>
2020-06-15 13:31:42 -0500

    Create 500.php

---
+++ view/500.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Motors</title>
+    <link href="/phpmotors/style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+  <header>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/header.php'; ?>
+  </header>
+
+  <main>
+<h1>Server Error</h1>
+<p>Sorry, our server seems to be experiencing some technical dificulties. Please check back later.</p>
+  </main>
+
+  <footer>
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/footer.php'; ?>
+  </footer>
+
+
+</body>
+</html>
